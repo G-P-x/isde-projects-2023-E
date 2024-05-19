f17f93cd3adfbb8091fc2fb009ee853400a7209a
miche9 <michelemereu94@gmail.com>
2024-05-19 12:16:31 +0200

    Fixed image_transformation.py

--- app/features/image_transformation.py
+++ app/features/image_transformation.py
@@ -1,31 +1,11 @@
 import os
-import uuid
-import time
 from PIL import Image, ImageEnhance
 from app.config import Configuration
 
 conf = Configuration()
 
 
-def cleanup_old_transformed_images(max_age_in_seconds):
-    current_time = time.time()
-
-    for filename in os.listdir(conf.image_folder_path):
-        file_path = os.path.join(conf.image_folder_path, filename)
-
-        # Ensure the file is an image and it's a transformed image
-        if not (filename.endswith('.JPEG') and 'transformed_image' in filename):
-            continue
-
-        # Get the modification time and delete the file if it's too old
-        file_modification_time = os.path.getmtime(file_path)
-        if current_time - file_modification_time > max_age_in_seconds:
-            os.remove(file_path)
-
-
 def change_image_parameters(image_id: str, color: float, brightness: float, contrast: float, sharpness: float):
-    cleanup_old_transformed_images(max_age_in_seconds=3600)  # Delete any old transformed images previously created
-
     params = {
         'Color': color / 10,
         'Brightness': brightness / 10,
@@ -37,8 +17,7 @@ def change_image_parameters(image_id: str, color: float, brightness: float, cont
         return image_id
 
     input_image_path = os.path.join(conf.image_folder_path, image_id)
-    unique_id = str(uuid.uuid4())
-    transformed_image_id = f"transformed_image_{unique_id}.JPEG"
+    transformed_image_id = f"{image_id.replace('_transformed', '').replace('.JPEG', '')}_transformed.JPEG"
     output_image_path = os.path.join(conf.image_folder_path, transformed_image_id)
     try:
         image = Image.open(input_image_path)
